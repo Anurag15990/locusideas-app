eb91e03be3c99678549481b890d343582169a622
Neha Yadav <nehayadav94@gmail.com>
2016-01-09 19:36:19 +0530

    - Add Asap font - Add Montserrat font - Add FontAwesome - Add Material Design Icons - Add Covered By Grace - Add SignInViewController - Add SignUpViewController - Add Sign In and Sign Up screens to Main.storyboard - Add logo to Assets.xcassets

---
+++ client/client/SignInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  client
+//
+//  Created by Neha Yadav on 06/01/16.
+//  Copyright © 2016 LocusIdeas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var signInEmail : UITextField!
+    @IBOutlet weak var signInPassword : UITextField!
+    @IBOutlet weak var forgotPassword : UIButton!
+    @IBOutlet weak var signInButton : UIButton!
+    @IBOutlet weak var orSignInWithLabel : UILabel!
+    @IBOutlet weak var facebookSignInView : UIView!
+    @IBOutlet weak var twitterLoginView : UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        signInButton.layer.cornerRadius = CGFloat(5)
+        
+        facebookSignInView.layer.borderWidth = 2
+        facebookSignInView.layer.cornerRadius = CGFloat(5)
+        facebookSignInView.layer.borderColor = UIColor .lightGrayColor().CGColor
+        
+        twitterLoginView.layer.borderWidth = 2
+        twitterLoginView.layer.cornerRadius = CGFloat(5)
+        twitterLoginView.layer.borderColor = UIColor .lightGrayColor().CGColor
+        
+    }
+    
+    
+}
---
+++ client/client/SignUpViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SignUpViewController.swift
+//  client
+//
+//  Created by Neha Yadav on 06/01/16.
+//  Copyright © 2016 LocusIdeas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUpViewController : UIViewController {
+    
+    @IBOutlet weak var signUpName : UITextField!
+    @IBOutlet weak var signUpEmail : UITextField!
+    @IBOutlet weak var signUpPassword : UITextField!
+    @IBOutlet weak var confirmPassword : UITextField!
+    @IBOutlet weak var signUpButton : UIButton!
+    @IBOutlet weak var orSignUpWithLabel : UILabel!
+    @IBOutlet weak var facebookSignUpView : UIView!
+    @IBOutlet weak var twitterSignUpView : UIView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        signUpButton.layer.cornerRadius = CGFloat(5)
+        
+        facebookSignUpView.layer.borderWidth = 2
+        facebookSignUpView.layer.cornerRadius = CGFloat(5)
+        facebookSignUpView.layer.borderColor = UIColor .lightGrayColor().CGColor
+        
+        twitterSignUpView.layer.borderWidth = 2
+        twitterSignUpView.layer.cornerRadius = CGFloat(5)
+        twitterSignUpView.layer.borderColor = UIColor .lightGrayColor().CGColor
+        
+//        for family: String in UIFont.familyNames()
+//        {
+//            print("\(family)")
+//            for names: String in UIFont.fontNamesForFamilyName(family)
+//            {
+//                print("== \(names)")
+//            }
+//        }
+        
+    }
+
+}
