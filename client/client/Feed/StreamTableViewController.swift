a645ac65d9e40bb7410f7154b216f82aa10c2ab5
Neha Yadav <nehayadav94@gmail.com>
2016-02-04 11:12:15 +0530

    - Change the Navigation Bar colour - Change the Back button color in the Navigation Bar

--- client/client/Feed/StreamTableViewController.swift
+++ client/client/Feed/StreamTableViewController.swift
@@ -19,11 +19,18 @@ class StreamTableViewController: UIViewController, UITableViewDataSource, UITabl
         
         tableView.registerNib(UINib(nibName: "StreamDesignCard", bundle: nil), forCellReuseIdentifier: "StreamDesignCard")
         tableView.separatorStyle = .None
+        
+        navigationController?.navigationBar.barTintColor = UIColor(red: 4.0/255.0, green: 158.0/255.0, blue: 143.0/255.0, alpha: 1.0)
+        
                 // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
     }
 
     override func didReceiveMemoryWarning() {
