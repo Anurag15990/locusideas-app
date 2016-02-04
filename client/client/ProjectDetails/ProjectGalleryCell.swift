fc57a57941a6130406c8de7fd2bd58b067c0270f
Neha Yadav <nehayadav94@gmail.com>
2016-02-04 19:49:09 +0530

    - Change the Status Bar Text Color to White - Change the App Orientation to Portrait - Add ProjectGalleryCell to ProjectDetails - Update Info.plist and AppDelegate.swift

---
+++ client/client/ProjectDetails/ProjectGalleryCell.swift
@@ -0,0 +1,26 @@
+//
+//  ProjectGalleryCell.swift
+//  client
+//
+//  Created by Neha Yadav on 04/02/16.
+//  Copyright © 2016 LocusIdeas. All rights reserved.
+//
+
+import UIKit
+
+class ProjectGalleryCell: UITableViewCell {
+
+    @IBOutlet weak var galleryImageView : UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
