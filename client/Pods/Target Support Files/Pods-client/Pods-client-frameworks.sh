e06f6813558fc31a636d43b1f51d3039b2023325
Anurag Agnihotri <anurag@getsuperapp.com>
2016-06-24 00:18:19 +0530

    Add Bond Framework for ViewModel Binding

--- client/Pods/Target Support Files/Pods-client/Pods-client-frameworks.sh
+++ client/Pods/Target Support Files/Pods-client/Pods-client-frameworks.sh
@@ -86,6 +86,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-client/Alamofire.framework"
   install_framework "Pods-client/Bolts.framework"
+  install_framework "Pods-client/Bond.framework"
   install_framework "Pods-client/Cloudinary.framework"
   install_framework "Pods-client/FBSDKCoreKit.framework"
   install_framework "Pods-client/FBSDKLoginKit.framework"
@@ -96,6 +97,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-client/Alamofire.framework"
   install_framework "Pods-client/Bolts.framework"
+  install_framework "Pods-client/Bond.framework"
   install_framework "Pods-client/Cloudinary.framework"
   install_framework "Pods-client/FBSDKCoreKit.framework"
   install_framework "Pods-client/FBSDKLoginKit.framework"
